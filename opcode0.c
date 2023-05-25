8388358a3d79708dc46e3c34c69fdf3f3a5ef900
Selasie-Freda <siesela88@gmail.com>
2023-05-25 07:32:04 -0700

    modified

--- opcode0.c
+++ opcode0.c
@@ -35,21 +35,3 @@ void f_push(stack_t **head, unsigned int counter)
 	else
 		addqueue(head, n);
 }
-
-
-/**
-* free_stack - frees a doubly linked list
-* @head: head of the stack
-*/
-void free_stack(stack_t *head)
-{
-	stack_t *aux;
-
-	aux = head;
-	while (head)
-	{
-		aux = head->next;
-		free(head);
-		head = aux;
-	}
-}
